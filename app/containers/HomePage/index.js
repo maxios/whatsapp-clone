49a65d9c4accc865c7e8ea8ad41aa43a2a383e42
maxios <maxios.code@gmail.com>
2017-07-28 19:56:49 +0200

    setting up the layout grid

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -1,24 +1,74 @@
-/*
- * HomePage
- *
- * This is the first thing users see of our App, at the '/' route
- *
- * NOTE: while this component should technically be a stateless functional
- * component (SFC), hot reloading does not currently support SFCs. If hot
- * reloading is not a necessity for you then you can refactor it and remove
- * the linting exception.
- */
 
 import React from 'react';
+import styled from 'styled-components'
 import { FormattedMessage } from 'react-intl';
 import messages from './messages';
+import ChatBackground from './chat-background.jpg';
+
+const values = {
+  TopBar: {
+    width: '60px',
+    color: '#eee'
+  },
+  Side: {
+    width: '400px',
+    color: 'white'
+  }
+}
+const App = styled.div`
+display: grid;
+grid-template-columns: ${values.Side.width} auto;
+grid-template-rows: ${values.TopBar.width} auto;
+grid-column-gap: 1px;
+width: 100vw;
+height: 100vh;
+`
+const Side = styled.div`
+grid-column: 1 / 2;
+grid-row: 2 / -1;
+background-color: ${values.Side.color};
+
+`
+const Body = styled.div`
+grid-column: 2 / 3;
+grid-row: 2 / -1;
+background: url('${ChatBackground}');
+`
+const TopBar = styled.div `
+grid-row: 1 / 2;
+grid-column: 1 / 2;
+background-color: ${values.TopBar.color};
+`
+const Options = styled.div `
+grid-row: 1 / 2;
+grid-column: 2 / -1;
+background-color: ${values.TopBar.color};
+`
+
 
 export default class HomePage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
+
+
   render() {
+
+   
     return (
-      <h1>
-        <FormattedMessage {...messages.header} />
-      </h1>
+      <div>
+      <App>
+        <TopBar>
+        </TopBar>
+
+        <Side>
+           
+        </Side>
+
+        <Options>
+        </Options>
+
+        <Body>
+        </Body>
+      </App>
+    </div>
     );
   }
 }
