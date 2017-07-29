c374c93ee703c3f65a9b48605d58e86bf1d4d885
maxios <maxios.code@gmail.com>
2017-07-29 22:14:37 +0200

    add essential AppBar Icons

--- app/containers/HomePage/AppBar.js
+++ app/containers/HomePage/AppBar.js
@@ -6,6 +6,9 @@ import IconMenu from 'material-ui/IconMenu';
 import MenuItem from 'material-ui/MenuItem';
 import FlatButton from 'material-ui/FlatButton';
 import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
+import CommunicationMessage from 'material-ui/svg-icons/communication/message';
+import FileAttachment from 'material-ui/svg-icons/file/attachment';
+import ActionSearch from 'material-ui/svg-icons/action/search';
 import NavigationClose from 'material-ui/svg-icons/navigation/close';
 
 
@@ -16,17 +19,29 @@ const Menu = (props) => (
       <IconButton><MoreVertIcon /></IconButton>
     }
     targetOrigin={{horizontal: 'right', vertical: 'top'}}
-    anchorOrigin={{horizontal: 'right', vertical: 'top'}}
+    anchorOrigin={{horizontal: 'right', vertical: 'bottom'}}
   >
     <MenuItem primaryText="Refresh" />
     <MenuItem primaryText="Help" />
     <MenuItem primaryText="Sign out" />
   </IconMenu>
 );
-/**
- * This example is taking advantage of the composability of the `AppBar`
- * to render different components depending on the application state.
- */
+
+const NewChat = (props) => (
+  <IconButton><CommunicationMessage /></IconButton>
+);
+
+const Attachment = (props) => (
+  <IconButton><FileAttachment /></IconButton>
+);
+
+const Search = (props) => (
+  <IconButton><ActionSearch /></IconButton>
+);
+
+
+
+
 class AppBarComponent extends Component {
   render() {
     var buttons = this.props.buttons
@@ -34,7 +49,7 @@ class AppBarComponent extends Component {
       <div>
         <AppBar
           iconElementLeft={<Avatar />}
-          iconElementRight={<Menu />}
+          iconElementRight={<div><Search /><Attachment /><NewChat /><Menu /></div>}
           style={{height: '100%', background: '#eee'}}
         />
       </div>
