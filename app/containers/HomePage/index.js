c5586aed251f9f270596138ef48b0ee8b81ccd24
maxios <maxios.code@gmail.com>
2017-07-31 02:43:49 +0200

    test AppBar rendering icon

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -1,81 +1,56 @@
-
-import React from 'react';
-import styled from 'styled-components'
-import { FormattedMessage } from 'react-intl';
-import messages from './messages';
-import ChatBackground from './chat-background.jpg';
-
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import Avatar from 'material-ui/Avatar'
+import React from 'react';
+import styled from 'styled-components';
+import Conversations from '../../components/Conversations';
+import AppBarComponent from '../../components/AppBar';
+import Input from '../../components/Input';
+import Message from '../../components/Message';
 
-import Conversations from './conversations.js';
-import AppBarComponent from './AppBar.js'
-
-const values = {
-  TopBar: {
-    width: '60px',
-    color: '#eee'
-  },
-  Side: {
-    width: '400px',
-    color: 'white'
-  }
-}
-const App = styled.div`
-display: grid;
-grid-template-columns: ${values.Side.width} auto;
-grid-template-rows: ${values.TopBar.width} auto;
-grid-column-gap: 1px;
-width: 100vw;
-height: 100vh;
-`
-
-const Side = styled.div`
-grid-column: 1 / 2;
-grid-row: 2 / -1;
-background-color: ${values.Side.color};
-`
-
-const Body = styled.div`
-grid-column: 2 / 3;
-grid-row: 2 / -1;
-background: url('${ChatBackground}');
-`
-
-const TopBar = styled(AppBarComponent) `
-grid-row: 1 / 2;
-grid-column: 1 / 2;
-background-color: ${values.TopBar.color};
-`
-
-const Options = styled(AppBarComponent) `
-grid-row: 1 / 2;
-grid-column: 2 / -1;
-background-color: ${values.TopBar.color};
-`
+// lets css griding the layout
+import {
+  App,
+  LeftSideTopBar,
+  RightSideTopBar,
+  ChatLists,
+  ChatBody,
+} from './Grid';
 
 
 export default class HomePage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
+  constructor(){
+    super();
+
+    this.state = {
+      conversations: [
+        {
+          user: 'khaled maher',
+          LastMessage: 'well, It\'s terrible idea to code after mid-night',
+          Unread: 1,
+        },
+      ]
+    }
+
+  }
 
   render() {
-   
     return (
-      <div>
-        <MuiThemeProvider>
-      <App>
-        <AppBarComponent />
+      <MuiThemeProvider>
+        <App>
+          <LeftSideTopBar Menu/>
+          <ChatLists>
+            <Conversations data={this.state.conversations}/>
+          </ChatLists>
 
-        <Side>
-          <Conversations />
-        </Side>
-
-        <Options />
-
-        <Body>
-        </Body>
-      </App>
-        </MuiThemeProvider>
-    </div>
+          <RightSideTopBar Menu Attachment Search/>
+          <ChatBody>
+            <Message float='left'/>
+            <Message float='right'/>
+            <Message float='right'/>
+            <Message float='right'/>
+          </ChatBody>
+          <Input />
+        </App>
+      </MuiThemeProvider>
     );
   }
 }
